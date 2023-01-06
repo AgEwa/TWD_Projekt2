39d2dc246616adb0a1fcfc83144cbd1e68977ba0
AgEwa <100518836+AgEwa@users.noreply.github.com>
2023-01-06 16:48:41 +0545

    Pierwszy zarys aplikacji

---
+++ AppSpotify/mainPage.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Spotify app"
+)
+
+st.title("What music do we listen to?")
+# st.sidebar.success("Select page above.")
+
+st.write("top artists, top sonds, music type (?)")
---
+++ AppSpotify/pages/page3.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import seaborn as sns
+import streamlit as st
+from matplotlib import pyplot as plt
+
+st.title("When and what do we listen to most often?")
+data = st.radio("Which data to display?", ["MyData", "Mg"])
+
+df = pd.read_json("./" + data + "/StreamingHistory0.json")
+df1 = pd.read_json("./" + data + "/StreamingHistory1.json")
+df = df.append(df1, ignore_index=True).query("msPlayed>30000")
+df["endTime"] = pd.to_datetime(df["endTime"])
+df["weekday"] = df["endTime"].dt.day_name()
+df["hour"] = df["endTime"].dt.hour
+df["count"] = df.groupby(["hour", "weekday"])["weekday"].transform('count')
+cats = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+df['weekday'] = pd.Categorical(df['weekday'], categories=cats, ordered=True)
+df = df.sort_values('weekday')
+print(df.sample(5))
+df.info()
+
+df1 = df[['hour', 'weekday', 'count']]
+x = pd.DataFrame(df1['weekday'].unique())
+heatmap_pt = pd.pivot_table(df1, values='count', index=['hour'], columns='weekday')
+fig, ax = plt.subplots(figsize=(16, 8))
+sns.set()
+sns.heatmap(heatmap_pt, cmap='YlGnBu')
+plt.xticks(rotation=15)
+
+st.pyplot(fig)
