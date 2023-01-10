c5bba778194c571965c15808cd91d5034dfe9228
Łukasz Lepianka <76851989+Luki308@users.noreply.github.com>
2023-01-10 18:55:01 +0100

    Resolved problems with file reading and showing plot

--- AppSpotify/pages/page2.py
+++ AppSpotify/pages/page2.py
@@ -4,7 +4,7 @@ import plotly.graph_objs as go
 
 st.title("Something with artists")
 
-df = pd.read_json('..\Lukasz\long\endsong_0.json')
+df = pd.read_json('./Lukasz/long/endsong_0.json')
 all_artists = len(df)
 
 artists_count = df.master_metadata_album_artist_name.value_counts().head(10)
@@ -18,9 +18,6 @@ label = df['artist']
 source = [0] * (len(df['artist']) - 1)
 target = [_ for _ in range(1, (len(df['artist'])))]
 value = df['count'].iloc[1:(len(df['artist']))].tolist()
-print(source)
-print(target)
-print(value)
 fig = go.Figure(data=[go.Sankey(
     node=dict(
         label=df['artist'],
@@ -34,4 +31,4 @@ fig = go.Figure(data=[go.Sankey(
 
 fig.update_layout(title_text="Top 10 artystów dla Łukasza", font_size=10)
 
-st.pyplot(fig)
+st.plotly_chart(fig)
