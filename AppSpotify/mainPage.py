ca4293c05fd089e2bb95a9d9b4b156bd2b935b8e
karim <01171252@pw.edu.pl>
2023-01-17 23:43:02 +0100

    update

--- AppSpotify/mainPage.py
+++ AppSpotify/mainPage.py
@@ -1,8 +1,10 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 import plotly.express as px
 
-df = pd.read_csv('karolina.csv', index_col = 0)
+# C:/Users/karim/PycharmProjects/TWD_Projekt2/AppSpotify/karolina.csv
+df = pd.read_csv("karolina.csv", index_col = 0)
 df['secPlayed'] = df['msPlayed'] / 1000
 df = df[df.columns[:-1].insert(4, df.columns[-1])]
 df = df[df.secPlayed > 60]
@@ -16,20 +18,21 @@ st.write("Check who are the artist we most frequently listen to. Maybe you know 
 st.write("What are the song's features we long for the most?\n"
          "Is valence more important then energy or maybe we want to listen to danceable songs?\n"
          "Find out!")
-hours = st.slider('Choose time range', 0, 24, (8,16))
+
+hours = st.slider('Choose time range for the first person', 0, 24, (8,16))
 
 df['datetime']=pd.to_datetime(df['datetime']).dt.hour
 df1=df[(df.datetime<=hours[1]) & (df.datetime>=hours[0])]
+
+if df1.empty:
+   df1=pd.DataFrame(0, index=np.arange(1, 2), columns=list(df.columns) )
+   df1["artistName"]="First person sleeps at this time"
+
 for feature in features:
     df1[f'{feature}_mean'] = df1[feature].mean()
 
 df1=df1.reset_index(drop = True)
 df2=df1.groupby("artistName").size().reset_index().rename(columns={0: 'Count'}).sort_values("Count", ascending=False).reset_index(drop = True).head(3)
-
-
-fig = px.line_polar(r=df1.loc[0,["danceability_mean","energy_mean","speechiness_mean","instrumentalness_mean","valence_mean"]], theta=features, line_close=True,template="plotly_dark")
-fig.update_traces(fill='toself')
-fig.update_polars(radialaxis_range=[0,1])
 
 fig = px.line_polar(r=df1.loc[0,["danceability_mean","energy_mean","speechiness_mean","instrumentalness_mean","valence_mean"]], theta=features, line_close=True)
 
@@ -46,20 +49,73 @@ with st.container():
         st.table(df2["artistName"])
     with col2:
         st.plotly_chart(fig, height=100)
+
+hoursL = st.slider('Choose time range for the second person', 0, 24, (8,16))
+
+dfLukasz=pd.read_csv('./Lukasz/final.csv', index_col = 0)
+dfLukasz['secPlayed'] = dfLukasz['msPlayed'] / 1000
+dfLukasz = dfLukasz[dfLukasz.columns[:-1].insert(4, dfLukasz.columns[-1])]
+dfLukasz = dfLukasz[dfLukasz.secPlayed > 60]
+features = ['danceability', 'energy', 'speechiness', 'instrumentalness', 'valence']
+
+dfLukasz['datetime']=pd.to_datetime(dfLukasz['datetime']).dt.hour
+dfLukasz1=dfLukasz[(dfLukasz.datetime<=hoursL[1]) & (dfLukasz.datetime>=hoursL[0])]
+if dfLukasz1.empty:
+   dfLukasz1=pd.DataFrame(0, index=np.arange(1, 2), columns=list(dfLukasz.columns) )
+   dfLukasz1["artistName"]="Second person sleeps at this time"
+for feature in features:
+    dfLukasz1[f'{feature}_mean'] = dfLukasz1[feature].mean()
+
+dfLukasz1=dfLukasz1.reset_index(drop = True)
+dfLukasz2=dfLukasz1.groupby("artistName").size().reset_index().rename(columns={0: 'Count'}).sort_values("Count", ascending=False).reset_index(drop = True).head(3)
+
+figLukasz = px.line_polar(r=dfLukasz1.loc[0,["danceability_mean","energy_mean","speechiness_mean","instrumentalness_mean","valence_mean"]], theta=features, line_close=True)
+
+# wstawić kolor
+figLukasz.update_layout(polar_bgcolor= 'white')
+figLukasz.update_traces(fill='toself')
+figLukasz.update_polars(radialaxis_range=[0,1])
+
 with st.container():
     col1, col2 = st.columns(2)
     with col1:
         st.title('Second incredible anonymous person ')
         st.write("Top artists")
-        st.table(df2["artistName"])
+        st.table(dfLukasz2["artistName"])
     with col2:
-        st.plotly_chart(fig,height=100)
+        st.plotly_chart(figLukasz,height=100)
+
+hoursA = st.slider('Choose time range for the third person', 0, 24, (8,16))
+dfAgata=pd.read_csv('./Lukasz/final.csv', index_col = 0)
+dfAgata['secPlayed'] = dfAgata['msPlayed'] / 1000
+dfAgata = dfAgata[dfAgata.columns[:-1].insert(4, dfAgata.columns[-1])]
+dfAgata = dfAgata[dfAgata.secPlayed > 60]
+features = ['danceability', 'energy', 'speechiness', 'instrumentalness', 'valence']
+
+dfAgata['datetime']=pd.to_datetime(dfAgata['datetime']).dt.hour
+dfAgata1=dfAgata[(dfAgata.datetime<=hoursA[1]) & (dfAgata.datetime>=hoursA[0])]
+if dfAgata1.empty:
+   dfAgata1=pd.DataFrame(0, index=np.arange(1, 2), columns=list(dfAgata.columns) )
+   dfAgata1["artistName"]="Third person sleeps at this time"
+for feature in features:
+    dfAgata1[f'{feature}_mean'] = dfAgata1[feature].mean()
+
+dfAgata1=dfAgata1.reset_index(drop = True)
+dfAgata2=dfAgata1.groupby("artistName").size().reset_index().rename(columns={0: 'Count'}).sort_values("Count", ascending=False).reset_index(drop = True).head(3)
+
+figAgata = px.line_polar(r=dfAgata1.loc[0,["danceability_mean","energy_mean","speechiness_mean","instrumentalness_mean","valence_mean"]], theta=features, line_close=True)
+
+# wstawić kolor
+figAgata.update_layout(polar_bgcolor= 'white')
+figAgata.update_traces(fill='toself')
+figAgata.update_polars(radialaxis_range=[0,1])
+
 with st.container():
     col1, col2 = st.columns(2)
     with col1:
         st.title('Third incredible anonymous person ')
         st.write("Top artists")
-        st.table(df2["artistName"])
+        st.table(dfAgata2["artistName"])
 
     with col2:
-        st.plotly_chart(fig,height=100)
+        st.plotly_chart(figAgata,height=100)
