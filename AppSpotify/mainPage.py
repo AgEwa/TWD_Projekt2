fab49e1c3deea5d2134851e650febf3a8b2b98e1
karim <01171252@pw.edu.pl>
2023-01-21 18:50:23 +0100

    update indices

--- AppSpotify/mainPage.py
+++ AppSpotify/mainPage.py
@@ -63,6 +63,7 @@ for feature in features:
 
 df1=df1.reset_index(drop = True)
 df2=df1.groupby("artistName").size().reset_index().rename(columns={0: 'Count'}).sort_values("Count", ascending=False).reset_index(drop = True).head(3)
+df2.index = df2.index + 1
 
 fig = px.line_polar(r=df1.loc[0,["danceability_mean","energy_mean","speechiness_mean","instrumentalness_mean","valence_mean"]], theta=features, line_close=True,color_discrete_sequence =['green']*3)
 
@@ -104,7 +105,7 @@ for feature in features:
 
 dfLukasz1=dfLukasz1.reset_index(drop = True)
 dfLukasz2=dfLukasz1.groupby("artistName").size().reset_index().rename(columns={0: 'Count'}).sort_values("Count", ascending=False).reset_index(drop = True).head(3)
-
+dfLukasz2.index = dfLukasz2.index + 1
 figLukasz = px.line_polar(r=dfLukasz1.loc[0,["danceability_mean","energy_mean","speechiness_mean","instrumentalness_mean","valence_mean"]], theta=features, line_close=True,color_discrete_sequence =['green']*3)
 
 # wstawić kolor
@@ -138,6 +139,7 @@ for feature in features:
 
 dfAgata1=dfAgata1.reset_index(drop = True)
 dfAgata2=dfAgata1.groupby("artistName").size().reset_index().rename(columns={0: 'Count'}).sort_values("Count", ascending=False).reset_index(drop = True).head(3)
+dfAgata2.index = dfAgata2.index + 1
 
 figAgata = px.line_polar(r=dfAgata1.loc[0,["danceability_mean","energy_mean","speechiness_mean","instrumentalness_mean","valence_mean"]], theta=features, line_close=True,color_discrete_sequence =['green']*3)
 
