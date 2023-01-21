c03e591c50c5ccec50cae4425676350fd69d0cc6
AgEwa <100518836+AgEwa@users.noreply.github.com>
2023-01-21 16:01:26 +0100

    Update mainPage.py

--- AppSpotify/mainPage.py
+++ AppSpotify/mainPage.py
@@ -5,11 +5,9 @@ import numpy as np
 import plotly.express as px
 from streamlit_extras.app_logo import add_logo
 
-#st.set_page_config(layout="wide")
 
 
 add_logo("./spotify.png", height=300)
-#st.write("👈 Check out the cat in the nav-bar!")
 
 page_bg_img = """
 <style>
@@ -29,7 +27,7 @@ black_font = """
 </style>
 """
 
-#st.sidebar.image(Image.open("./spotify.png"),use_column_width=True)
+
 with st.sidebar:
     st.markdown(page_bg_img,unsafe_allow_html=True)
     st.markdown(black_font, unsafe_allow_html=True)
