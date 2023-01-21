e825eaed9f621338eb39af53d783be5411c1fcef
Łukasz Lepianka <76851989+Luki308@users.noreply.github.com>
2023-01-21 02:35:04 +0100

    page2 - Cumulate 3rd column, change links' colors

--- AppSpotify/pages/page2.py
+++ AppSpotify/pages/page2.py
@@ -1,17 +1,20 @@
 import streamlit as st
 import pandas as pd
 import plotly.graph_objs as go
+import plotly.express as px
+import matplotlib
 
-st.title("Which artists we share the most?")
+st.title("How much do we share listening to our favourite artists?")
 
 df1 = pd.read_json('./Lukasz/long/endsong_0.json')
 df2 = pd.read_json('./Agata/extended/endsong_0.json')
 df3 = pd.read_json('./Karolina/endsong.json')
 
 artist_chosen = st.selectbox("Choose an artist",
-             ["Adele", "The Dumplings"])
+             ["Adele", "The Dumplings","Maanam", "Michael Jackson", "Daria Zawiałow"])
 
 names = ["Łukasz", "Agata", "Karolina"]
+colors = ["#1db954","#083318","#10642d"]
 frames = [df1, df2, df3]
 df_plot_year = None
 df_plot_name = None
@@ -41,21 +44,32 @@ for df in frames:
 
     year_len.append(len(df_filter_by_artist))
 
+df_plot_year = df_plot_year.reset_index(drop=True)
+min_year = int(df_plot_year['Year'].min())
+max_year = int(df_plot_year['Year'].max())
+years = list(range(min_year, max_year+1))
+
 # Data for Sankey diagram
-label = ['Sum of all tracks'] + df_plot_name['Name'].tolist() + df_plot_year['Year'].tolist()
+label = ['Sum of all tracks'] + df_plot_name['Name'].tolist() + years
 source = []
 target = []
 value = []
+color = []
 for i in range(len(df_plot_name)):
     source += [0]
     target += [i + 1]
+    color += [colors[i]]
+    print(color)
 value += df_plot_name['count'].tolist()
-before = 0
+
+chosen_year = 0
 for i in range(len(df_plot_name)):
     for j in range(year_len[i]):
         source += [1 + i]
-        target += [1 + len(df_plot_name) + j + before]
-    before += year_len[i]
+        target += [label.index(int(df_plot_year.loc[chosen_year+j,'Year']))]
+        color += [colors[i]]
+        print(color)
+    chosen_year += year_len[i]
 value += df_plot_year['count'].tolist()
 
 # node_x = [0]+[1]*len(df_plot_name)+[2]*len(df_plot_year)
@@ -66,17 +80,20 @@ value += df_plot_year['count'].tolist()
 
 fig = go.Figure(data=[go.Sankey(
     node=dict(
-        label=label
+        label=label,
+        color = px.colors.qualitative.Light24
         # x = node_x,
-        # y = node_y
+        # y = node_y,
+
     ),
     link=dict(
-        source=source,
-        target=target,
-        value=value
+        source = source,
+        target = target,
+        value = value,
+        color = color
     )
 )])
-
+#color = "#1db954"
 fig.update_layout(title_text=" Sankey plot for chosen artist ", font_size=18)
 
 st.plotly_chart(fig)
